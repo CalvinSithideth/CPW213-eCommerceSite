c1ec63161e0324430469a5e6d29039ce6349346e
CalvinSithideth <calvin.sithideth@gmail.com>
2019-08-20 06:34:29 -0700

    Created functional registration

---
+++ eCommerce/Data/MemberDB.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    public static class MemberDB
+    {
+        /// <summary>
+        /// Adds a member to the database. Returns the member
+        /// with their MemberID populated
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public async static Task<Member> Add(GameContext context, Member m)
+        {
+            context.Members.Add(m);
+            await context.SaveChangesAsync();
+            return m;
+        }
+    }
+}
